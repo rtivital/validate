2e8360ee429dcda4bd7266863171f81ea444ce24
rtivital <rtivital@gmail.com>
2016-02-09 18:39:09 +0300

    Huge update

--- gulpfile.js
+++ gulpfile.js
@@ -1,34 +1,18 @@
 var gulp = require('gulp');
-var sass = require('gulp-sass');
 var uglify = require('gulp-uglify');
-var minifyCss = require('gulp-minify-css');
 var rename = require("gulp-rename");
+var eslint = require('gulp-eslint');
 
-// Compile Sass
-gulp.task('sass', function () {
-  gulp.src('./scss/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./'));
-});
- 
-gulp.task('sass:watch', function () {
-  gulp.watch('./scss/**/*.scss', ['sass']);
+gulp.task('lint', function () {
+  return gulp.src(['validate.js'])
+    .pipe(eslint())
+    .pipe(eslint.format())
+    .pipe(eslint.failAfterError());
 });
 
-// Minify CSS
-gulp.task('minify-css', ['sass'], function() {
-  return gulp.src('iv.css')
-    .pipe(minifyCss({ compatibility: 'ie9' }))
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
-});
-
-// Minify Library
-gulp.task('minify-lib', function() {
+gulp.task('default', ['lint'], function() {
   return gulp.src('validate.js')
     .pipe(uglify())
     .pipe(rename({ suffix: '.min' }))
-    .pipe(gulp.dest('./dist'));
+    .pipe(gulp.dest('./'));
 });
-
-gulp.task('build', ['minify-css', 'minify-lib']);
--- validate.js
+++ validate.js
@@ -1,262 +1,140 @@
-(function () {
-	'use strict';
+var Validator = (function() {
+  'use strict';
 
-	var geId = function(el) {
-		return  document.getElementById(el);
-	};
+  // Creates messages by replacement %var% templates
+  var _createMessage = function(message, settings) {
+    for (var key in settings) {
+      message = message.replace('%' + key + '%', settings[key]);
+    }
+    return message;
+  };
 
-	var query = function(el, selector) {
-		var container = el || document;
-		return container.querySelector(selector);
-	};
+  // Returns is provided param is actually an input element
+  var _isInput = function(element) {
+    if (!element || !element.tagName) {
+      return false;
+    }
+    var tagName = element.tagName.toLowerCase()
+    return tagName === 'input' || tagName === 'textarea';
+  };
 
-	var createElement = function(tag) {
-		return document.createElement(tag);
-	};
+  // Extends one object from others
+  // http://youmightnotneedjquery.com/#deep_extend
+  var _extend = function(out) {
+    out = out || {};
 
-	var _select = function(el) {
-		if (typeof el === 'string') {
-			var isId = el.charAt(0) === '#'
-			var selector = el.slice(1);
-			var result = isId ? geId(selector) : query(el);
-		}
-		return result || el;
-	};
+    for (var i = 1; i < arguments.length; i++) {
+      var obj = arguments[i];
 
-	var _isInput = function(el) {
-		return ['input', 'textarea', 'select'].some(function(tagName) {
-			return el.tagName.toLowerCase() === tagName;
-		});
-	};
+      if (!obj)
+        continue;
 
-	var _isString = function(str) {
-		return typeof str === 'string' ? str.trim().length > 0 : false;
-	};
+      for (var key in obj) {
+        if (obj.hasOwnProperty(key)) {
+          if (typeof obj[key] === 'object')
+            out[key] = _extend(out[key], obj[key]);
+          else
+            out[key] = obj[key];
+        }
+      }
+    }
 
-	var _isNumber = function(num) {
-		return typeof value === 'number' && isFinite(value) && Math.floor(value) === value;
-	};
+    return out;
+  };
 
-	var _isArray = function(arr) {
-		return Array.isArray(arr);
-	};
+  // Basic regular expressions
+  var regExps = {
+    email: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i,
+    url: /^((https?):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
+    numbers: /^\d+(\.\d{1,2})?$/,
+    digit: /[0-9]*$/,
+    letters: /[a-z][A-Z]*$/
+  };
 
-	var _isObject = function(obj) {
-		var type = typeof obj;
-		return type === 'function' || type === 'object' && !!obj;
-	};
+  // Default messages: All object created with Validate function will share link to this object
+  var messages = {
+    required: 'This field is required',
+    min: 'This field should contain at least %rule% characters',
+    max: 'This field should not contain more than %rule% characters',
+    match: 'This field shold countain a valid %rule%'
+  };
 
-	var _isRegExp = function(regexp) {
-		return regexp instanceof RegExp;
-	};
+  var Validate = function(element, options) {
+    if (!_isInput(element)) {
+      throw new Error('Only value from <input> and <textarea> can be validated');
+    }
 
-	var _extend = function() {
-		var args = arguments;
-		var i = args.length;
-		var result = args[0];
+    if (!options.rules || Object.keys(options.rules).length === 0) {
+      throw new Error('No rules for validation were passed to Validator function');
+    }
 
-		while ( i-- ) {
-			if (i === 0) break;
-			var obj = args[i];
-			if (_isObject(obj)) {
-				for (var key in obj) {
-					if (result.hasOwnProperty(key) && _isObject(obj[key])) {
-						_extend(result[key], obj[key]);
-					} else {
-						result[key] = obj[key];
-					}
-				}
-			}
-		}
-		return result;
-	};
+    var defaults = {
+      regExps: regExps,
+      messages: messages
+    };
+    // Set basic options to get access from onSuccess and onError functions
+    this.options = _extend({}, defaults, options);
+    this.element = element;
 
-	var _createMessage = function(str, substr) {
-		if (_isNumber(substr)) { substr = substr.toString(); }
-		return (_isString(str) && _isString(substr))
-			? str.replace('%s', substr) 
-			: str;
-	};
+    // Set regExps from outer scope
+    this.regExps = regExps;
+  };
 
-	var _wrapMessage = function(tag, classes, text) {
-		var el = createElement(tag);
-		el.className = classes;
-		el.textContent = text;
-		return el;
-	};
+  var fn = Validate.prototype;
 
-	var _replaceClass = function(el, oldClass, newClass) {
-		var classes = el.classList;
-		classes.remove(oldClass);
-		classes.add(newClass);
-		return el;
-	};
+  fn.validate = function() {
+    var isValid = true;
 
-	var _contain = function(item) {
-		return this.value.indexOf(item) > -1;
-	};
+    this.value = this.element.value;
+    for (var rule in this.options.rules) {
+      // Set testing function
+      var test = this[rule];
 
-	var messages, settings, classes, regexps, errorsContainer, errors;
+      if (!test || typeof test !== 'function') {
+        throw new Error('Rule ' + rule + ' can\'t be evaluated. It is not predefined. You can create it yourself with Validator.fn');
+      }
 
-	var Validate = function(input, user_settings) {
-		settings = {
-			messages: {
-				min: 'This field should contain at least %s charachters',
-				max: 'This field should not contain more than %s charachters',
-				required: 'This field is required',
-				match: 'This field should match this pattern "%s"',
-				contain: 'This field sholud contain these charachters %s'
-			},
-			regexps: {
-				ip: /^((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[0-9]{1,2})$/i,
-				url: /^((https?):\/\/(\w+:{0,1}\w*@)?(\S+)|)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?$/,
-				date: /\d{1,2}-|.\d{1,2}-|.\d{4}/,
-				base64: /[^a-zA-Z0-9\/\+=]/i,
-				email: /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i
-			},
-			classes: {
-				message: 'iv-message',
-				errorsContainer: 'iv-errors-container',
-				inputSuccess: 'iv-success',
-				inputError: 'iv-error'
-			}
-		};
+      // Grab param from options object
+      var param = this.options.rules[rule];
 
-		_extend(settings, user_settings);
+      if (!this[rule](param)) {
+        isValid = false;
+        this.message = _createMessage(this.options.messages[rule], {rule: param, data: this.value});
+        this.options.onError.call(this);
+        break;
+      }
+    }
 
-		messages = settings.messages;
-		classes = settings.classes;
-		regexps = settings.regexps;
+    if (isValid) {
+      this.options.onSuccess.call(this);
+    }
+  };
 
-		var input = this.input = _select(input);
-		var parentNode = input.parentNode;
-		var classErrorsContainer = classes.errorsContainer
-		var domErrorsContainer = query(parentNode, '.' + classErrorsContainer);
+  fn.required = function() {
+    return this.value.trim().length > 0;
+  };
 
-		this.value = input.value;
-		errors = [];
+  fn.min = function(param) {
+    return this.value.length >= param;
+  };
 
-		if (!domErrorsContainer) {
-			errorsContainer = createElement('div');
-			errorsContainer.classList.add(classErrorsContainer);
-			parentNode.insertBefore(errorsContainer, input);
-		} else {
-			errorsContainer = domErrorsContainer;
-			errorsContainer.innerHTML = '';
-		}
-	};
+  fn.max = function(param) {
+    return this.value.length <= param;
+  };
 
-	Validate.prototype.test = function(fn, message) {
-		if (!fn.call(this)) {
-			errors.push(message);
-		}
-		return this;
-	};
+  fn.match = function(param) {
+    var re = this.regExps[param];
+    if (!re) {
+      console.warn('There are no such predefined regexp: '
+        + param + '. All predefined regExps are: '
+        + Object.keys(this.options.regExps).join(', ') +'. Data is assumed to be valid.');
+      return true;
+    }
+    return this.regExps[param].test(this.value);
+  };
 
-	Validate.prototype.required = function(message) {
-		message = message || messages['required'];
-		return this.test(function() {
-			return this.value.toString().trim().length > 0;
-		}, message);
-	};
-
-	Validate.prototype.min = function(length, message) {
-		message = message || messages['min'];
-		return this.test(function() {
-			return this.value.length >= length;
-		}, _createMessage(message, length));
-	};
-
-	Validate.prototype.max = function(length, message) {
-		message = message || messages['max'];
-		return this.test(function() {
-			return this.value.length <= length;
-		}, _createMessage(message, length));
-	};
-
-	Validate.prototype.match = function(pattern, message) {
-		message = message || messages['match'];
-
-		// Figure out if passed pattern is regexp
-		// in that case use provided regexp
-		// Otherwize try to grab regexp from settings
-		var regexp;
-		if (_isRegExp(pattern)) {
-			regexp = pattern;
-		} else if (regexps[pattern]) {
-			regexp = regexps[pattern];
-		} else {
-			console.warn('Match method works only with regular expression or one of these presets: ' + Object.keys(regexps).join(', '));
-			return this;
-		}
-		return this.test(function() {
-			return regexps[pattern].test(this.value);
-		}, _createMessage(message, pattern))
-	};
-
-	Validate.prototype.contain = function(chars, message) {
-		message = message || messages['contain'];
-
-		// If array was passed to function cycle through all given charachters
-		// and figure out if all of them are contained in this.value
-		// Otherwize check if given string is contained in this.value
-		return this.test(function() {
-			return _isArray(chars) ? chars.every(_contain, this) : _contain.call(this, chars);
-		}, _createMessage(message, _isArray(chars) ? chars.join(', ') : chars));
-	};
-
-	Validate.prototype.showErrors = function(index) {
-		// Make sure that all previous messages were deleted
-		errorsContainer.innerHTML = '';
-
-		// If number was passed to function show only one error
-		// Otherwize show all existing errors
-		_isNumber(index) 
-			? errorsContainer.appendChild(_wrapMessage('p', classes.message, errors[index]))
-			: errors.forEach(function(error) {
-				errorsContainer.appendChild(_wrapMessage('p', classes.message, error));
-			}, this);
-		return this;
-	};
-
-	// Return if all tests were passed
-	Validate.prototype.isValid = function() {
-		return errors.length === 0;
-	};
-
-	// Remove all previous messages from errors array
-	Validate.prototype.clear = function() {
-		errors = [];
-		return this;
-	};
-
-	Validate.prototype.error = function() {
-		_replaceClass(this.input, classes.inputSuccess, classes.inputError);
-		return this;
-	};
-
-	Validate.prototype.success = function() {
-		_replaceClass(this.input, classes.inputError, classes.inputSuccess);
-		return this;
-	};
-
-	Validate.prototype.onError = function(fn) {
-		if (typeof fn === 'function' && !this.isValid()) {
-			fn.call(this);
-		}
-		return this;
-	};
-
-	Validate.prototype.onSuccess = function(fn) {
-		if (typeof fn === 'function' && this.isValid()) {
-			fn.call(this);
-		}
-		return this;
-	};
-
-	var validate = function(input) {
-		return new Validate(input);
-	};
-
-	window.validate = validate;
+  return {
+    init: Validate,
+    fn: fn
+  };
 })();
